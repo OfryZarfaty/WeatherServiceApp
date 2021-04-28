a9b59858b18236a7a8bc13ddce61a8b052070562
OfryZarfaty <ofryzarfaty@gmail.com>
2021-04-28 14:27:57 +0300

    upload weather app

---
+++ data/db_init.js
@@ -0,0 +1,104 @@
+const { Client } = require('pg');
+const fs = require("fs");
+const { release } = require('os');
+const copyFrom = require('pg-copy-streams').from
+const config = require('config');
+
+/*CONNECT TO DATABASE LOCALY*/
+// const client = new Client({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'testdb',
+//     password: 'postgres',
+//     port: 5432,
+// });
+
+/*CONNECT TO HEROKU DATABASE*/
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+/*DELETE EXISTING TABLES*/
+client.query(`
+DROP TABLE IF EXISTS file1, file2, file3;
+`, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Tables successfully deleted');
+});
+
+/*CREATE TABLES*/
+client.query(config.get('db.createTableOneQuery'),(err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Table 1 is successfully created');
+});
+
+client.query(config.get('db.createTableTwoQuery'), (err, res) => {
+  release();
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Table 2 is successfully created');
+});
+
+client.query(config.get('db.createTableThreeQuery'), (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Table 3 is successfully created');
+});
+
+/*INPUTFILES AND TARGET TABLES*/
+var targetTableOne = 'file1';
+var targetTableTwo = 'file2';
+var targetTableThree = 'file3';
+
+const executeQuery = (targetTable) => {
+  const execute = (target, callback) => {
+      client.query(`Truncate ${target}`, (err) => {
+              if (err) {
+                console.log(`Error in creating read stream ${err}`);
+                 client.end();
+                callback(err);
+              } else {
+              console.log(`Truncated ${target}`);
+              callback(null, target);
+              }
+          });
+  }
+
+  execute(targetTable, (err) =>{
+      if (err) return console.log(`Error in Truncate Table: ${err}`)
+      var stream = client.query(copyFrom(`COPY ${targetTable} (Longitude,Latitude,forecasttime,Temperature,Precipitation) FROM STDIN WITH CSV HEADER`));
+      var fileStream = fs.createReadStream(__dirname + '/' + targetTable + '.csv');
+
+      fileStream.on('error', (err) =>{
+          console.log(`Error in creating read stream ${err}`)
+      });
+
+      stream.on('finish', () => {
+        console.log(`Completed loading data into ${targetTable}`)
+        if(targetTable == targetTableThree){
+          client.end()
+        }
+    });
+      fileStream.pipe(stream);
+  });  
+};
+
+/*POPULATE TABLES*/
+executeQuery(targetTableOne);
+executeQuery(targetTableTwo);
+executeQuery(targetTableThree);
---
+++ main.js
@@ -0,0 +1,88 @@
+const express=require('express');
+const { Client } = require('pg');
+const path=require('path');
+const app=express();
+const PORT = process.env.PORT || 3000;
+const config = require('config');
+
+/*APP SETUP*/
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'/views'));
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req,res) => {
+    res.render('index.ejs');
+});
+
+/*WEATHER/DATA API*/
+app.get('/weather/data', async (req, res) => {
+  var Latitude =req.query.lat;
+  var Longitude = req.query.lan;
+  const client = new Client({
+      // user: 'postgres',
+      // host: 'localhost',
+      // database: 'testdb',
+      // password: 'postgres',
+      // port: 5432
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }    
+  }); 
+  client.connect();
+  const result1 = await client.query(config.get('app.weatherDataFileOneQuery'), [Longitude,Latitude]);
+  const result2 = await client.query(config.get('app.weatherDataFileTwoQuery'), [Longitude,Latitude]);
+  const result3 = await client.query(config.get('app.weatherDataFileThreeQuery'), [Longitude,Latitude]);
+  res.json([result1.rows[0], result2.rows[0], result3.rows[0]]);
+});
+
+/*WEATHER/SUMMARIZE API*/
+app.get('/weather/summarize', async (req, res) => {
+  var Latitude =req.query.lat;
+  var Longitude = req.query.lan;
+  const client = new Client({
+      // user: 'postgres',
+      // host: 'localhost',
+      // database: 'testdb',
+      // password: 'postgres',
+      // port: 5432
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }    
+  }); 
+  client.connect();
+
+  const queryMin = 'select MIN(temperature) as temperature, MIN(Precipitation) as Precipitation from (\n' +
+  'select temperature,Precipitation from file1 \n' +
+	'WHERE Longitude=$1 and Latitude=$2\n' +
+	'UNION\n' +
+	'select temperature,Precipitation from file2\n' +
+	'WHERE Longitude=$3 and Latitude=$4\n' +
+	'UNION\n' +
+	'select temperature,Precipitation from file3\n' +
+	'WHERE Longitude=$5 and Latitude=$6 ) as all_tables';
+
+  const queryMax = 'select MAX(temperature) as temperature, MAX(Precipitation) as Precipitation from (\n' +
+  'select temperature,Precipitation from file1 \n' +
+	'WHERE Longitude=$1 and Latitude=$2\n' +
+	'UNION\n' +
+	'select temperature,Precipitation from file2\n' +
+	'WHERE Longitude=$3 and Latitude=$4\n' +
+	'UNION\n' +
+	'select temperature,Precipitation from file3\n' +
+	'WHERE Longitude=$5 and Latitude=$6 ) as all_tables';
+
+  const min = await client.query(queryMin, [Longitude,Latitude,Longitude,Latitude,Longitude,Latitude]);
+  const max = await client.query(queryMax, [Longitude,Latitude,Longitude,Latitude,Longitude,Latitude]);
+  const temperature = ((min.rows[0].temperature)+(max.rows[0].temperature))/2;
+  const precipitation = ((min.rows[0].precipitation)+(max.rows[0].precipitation))/2;
+  res.json({max: max.rows[0],
+            min: min.rows[0],
+            avg: {temperature, precipitation}});
+});
+
+app.listen(PORT, () =>{
+    console.log("listening on port");
+});
+
